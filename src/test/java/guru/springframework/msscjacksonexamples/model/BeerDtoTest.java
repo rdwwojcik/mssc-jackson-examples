0b62d5a23bf3a2386425f946c824b7c1ff4f85b1
radek <rdwwojcik@gmail.com>
2023-06-15 15:11:51 +0200

    #9.110 Setting Property Names with Jackson

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -1,14 +1,10 @@
 package guru.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by radek on 2023-06-15
@@ -27,7 +23,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserialize() throws IOException {
-        String json = "{\"id\":\"a9ada52d-3810-4aa1-abd4-f60ed843f155\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123112313,\"price\":null,\"createdDate\":\"2023-06-15T14:31:24.80467796+02:00\",\"lastUpdatedDate\":\"2023-06-15T14:31:24.80540692+02:00\"}";
+        String json = "{\"beerId\":\"a9ada52d-3810-4aa1-abd4-f60ed843f155\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123112313,\"price\":null,\"createdDate\":\"2023-06-15T14:31:24.80467796+02:00\",\"lastUpdatedDate\":\"2023-06-15T14:31:24.80540692+02:00\"}";
 
         BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
 
